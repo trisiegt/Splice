85b8c7993adf7215552ecd6d5c406eae9cc53938
Tristan Carpenter <77842798+trisiegt@users.noreply.github.com>
2023-07-25 18:38:39 +0100

    Add the source code (VS 2022, .net 7.0)

---
+++ Splice/Splice/Form1.Designer.vb
@@ -0,0 +1,432 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        CheckBox3 = New CheckBox()
+        Label9 = New Label()
+        CheckBox2 = New CheckBox()
+        Label8 = New Label()
+        TabPage2 = New TabPage()
+        CheckBox5 = New CheckBox()
+        Label11 = New Label()
+        CheckBox4 = New CheckBox()
+        Label10 = New Label()
+        CheckBox1 = New CheckBox()
+        Label7 = New Label()
+        TabPage3 = New TabPage()
+        CheckBox8 = New CheckBox()
+        Label14 = New Label()
+        CheckBox7 = New CheckBox()
+        Label13 = New Label()
+        CheckBox6 = New CheckBox()
+        Label12 = New Label()
+        TabPage4 = New TabPage()
+        CheckBox10 = New CheckBox()
+        Label16 = New Label()
+        CheckBox9 = New CheckBox()
+        Label15 = New Label()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
+        TabPage3.SuspendLayout()
+        TabPage4.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(146, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Splice® Client build 23725"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(527, 9)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Logged in as:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(619, 9)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(60, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Username"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Microsoft JhengHei UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(38, 66)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(160, 35)
+        Label4.TabIndex = 3
+        Label4.Text = "Categories:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(522, 426)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(266, 15)
+        Label5.TabIndex = 8
+        Label5.Text = "Programmed by Tristan Carpenter|Trisiegt © 2023"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(29, 426)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(132, 15)
+        Label6.TabIndex = 9
+        Label6.Text = "Steam_Installation_Path"
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Controls.Add(TabPage3)
+        TabControl1.Controls.Add(TabPage4)
+        TabControl1.Location = New Point(29, 104)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(742, 319)
+        TabControl1.TabIndex = 10
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(CheckBox3)
+        TabPage1.Controls.Add(Label9)
+        TabPage1.Controls.Add(CheckBox2)
+        TabPage1.Controls.Add(Label8)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(734, 291)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Visual"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(286, 57)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(15, 14)
+        CheckBox3.TabIndex = 5
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(6, 57)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(272, 15)
+        Label9.TabIndex = 4
+        Label9.Text = "Twerk (spams crouch, only visible to other players)"
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(236, 18)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(15, 14)
+        CheckBox2.TabIndex = 3
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(6, 18)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(224, 15)
+        Label8.TabIndex = 2
+        Label8.Text = "ESP (see items and players through walls)"
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Controls.Add(CheckBox5)
+        TabPage2.Controls.Add(Label11)
+        TabPage2.Controls.Add(CheckBox4)
+        TabPage2.Controls.Add(Label10)
+        TabPage2.Controls.Add(CheckBox1)
+        TabPage2.Controls.Add(Label7)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(734, 291)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "Weapons"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(86, 83)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(15, 14)
+        CheckBox5.TabIndex = 5
+        CheckBox5.UseVisualStyleBackColor = True
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(15, 83)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(48, 15)
+        Label11.TabIndex = 4
+        Label11.Text = "KillAura"
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(86, 46)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(15, 14)
+        CheckBox4.TabIndex = 3
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(15, 47)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(47, 15)
+        Label10.TabIndex = 2
+        Label10.Text = "Aimbot"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(86, 15)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(15, 14)
+        CheckBox1.TabIndex = 1
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(15, 15)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(55, 15)
+        Label7.TabIndex = 0
+        Label7.Text = "Wallhack"
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.Controls.Add(CheckBox8)
+        TabPage3.Controls.Add(Label14)
+        TabPage3.Controls.Add(CheckBox7)
+        TabPage3.Controls.Add(Label13)
+        TabPage3.Controls.Add(CheckBox6)
+        TabPage3.Controls.Add(Label12)
+        TabPage3.Location = New Point(4, 24)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Padding = New Padding(3)
+        TabPage3.Size = New Size(734, 291)
+        TabPage3.TabIndex = 2
+        TabPage3.Text = "Inventory"
+        TabPage3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox8
+        ' 
+        CheckBox8.AutoSize = True
+        CheckBox8.Location = New Point(133, 91)
+        CheckBox8.Name = "CheckBox8"
+        CheckBox8.Size = New Size(15, 14)
+        CheckBox8.TabIndex = 5
+        CheckBox8.UseVisualStyleBackColor = True
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(22, 90)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(110, 15)
+        Label14.TabIndex = 4
+        Label14.Text = "All Skins (C and CT)"
+        ' 
+        ' CheckBox7
+        ' 
+        CheckBox7.AutoSize = True
+        CheckBox7.Location = New Point(133, 60)
+        CheckBox7.Name = "CheckBox7"
+        CheckBox7.Size = New Size(15, 14)
+        CheckBox7.TabIndex = 3
+        CheckBox7.UseVisualStyleBackColor = True
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(22, 59)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(105, 15)
+        Label13.TabIndex = 2
+        Label13.Text = "Infinite Name Tags"
+        ' 
+        ' CheckBox6
+        ' 
+        CheckBox6.AutoSize = True
+        CheckBox6.Location = New Point(224, 31)
+        CheckBox6.Name = "CheckBox6"
+        CheckBox6.Size = New Size(15, 14)
+        CheckBox6.TabIndex = 1
+        CheckBox6.UseVisualStyleBackColor = True
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(16, 30)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(199, 15)
+        Label12.TabIndex = 0
+        Label12.Text = "All Decals (unlocks all weapon skins)"
+        ' 
+        ' TabPage4
+        ' 
+        TabPage4.Controls.Add(CheckBox10)
+        TabPage4.Controls.Add(Label16)
+        TabPage4.Controls.Add(CheckBox9)
+        TabPage4.Controls.Add(Label15)
+        TabPage4.Location = New Point(4, 24)
+        TabPage4.Name = "TabPage4"
+        TabPage4.Padding = New Padding(3)
+        TabPage4.Size = New Size(734, 291)
+        TabPage4.TabIndex = 3
+        TabPage4.Text = "Misc."
+        TabPage4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox10
+        ' 
+        CheckBox10.AutoSize = True
+        CheckBox10.Location = New Point(167, 50)
+        CheckBox10.Name = "CheckBox10"
+        CheckBox10.Size = New Size(15, 14)
+        CheckBox10.TabIndex = 3
+        CheckBox10.UseVisualStyleBackColor = True
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Location = New Point(23, 51)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(136, 15)
+        Label16.TabIndex = 2
+        Label16.Text = "Custom Menu Backdrop"
+        ' 
+        ' CheckBox9
+        ' 
+        CheckBox9.AutoSize = True
+        CheckBox9.Location = New Point(241, 22)
+        CheckBox9.Name = "CheckBox9"
+        CheckBox9.Size = New Size(15, 14)
+        CheckBox9.TabIndex = 1
+        CheckBox9.UseVisualStyleBackColor = True
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(22, 21)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(213, 15)
+        Label15.TabIndex = 0
+        Label15.Text = "ForceOP (Forces admin on ALL servers)"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TabControl1)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Splice(R) For CS:GO"
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage1.PerformLayout()
+        TabPage2.ResumeLayout(False)
+        TabPage2.PerformLayout()
+        TabPage3.ResumeLayout(False)
+        TabPage3.PerformLayout()
+        TabPage4.ResumeLayout(False)
+        TabPage4.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents CheckBox10 As CheckBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents CheckBox9 As CheckBox
+    Friend WithEvents Label15 As Label
+End Class
---
+++ Splice/Splice/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Message, Title, UsernamePrompt
+        Message = "What is your Steam username?"    ' Set prompt.
+        Title = "Splice for CSGO"    ' Set title.
+
+        ' Display message, title, and default value.
+        UsernamePrompt = InputBox(Message, Title, )
+
+        Label3.Text = UsernamePrompt
+        If Label3.Text = "" Then Close()
+
+        Dim SteamPath = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam", "InstallPath", Nothing)
+        Label6.Text = "Steam Install Path (required for patching): " & SteamPath
+
+    End Sub
+
+
+End Class
